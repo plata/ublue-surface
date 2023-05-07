fa8e4b6f85e311cda161c1a57a18c46bf5371c29
plata <plata@users.noreply.github.com>
2023-05-07 17:37:56 +0200

    Fix SDDM scaling (#16)
    If `QT_SCREEN_SCALE_FACTORS` is set, Maliit doesn't show up anymore.

--- scripts/configure_sddm.sh
+++ scripts/configure_sddm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's/layer-shell/layer-shell,QT_SCREEN_SCALE_FACTORS=2,QT_FONT_DPI=192/g' /usr/lib/sddm/sddm.conf.d/plasma-wayland.conf
+sed -i 's/layer-shell/layer-shell,QT_FONT_DPI=144/g' /usr/lib/sddm/sddm.conf.d/plasma-wayland.conf
