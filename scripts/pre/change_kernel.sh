60142141ad60e23be56e1d75cb2e62f0996726a7
plata <plata@users.noreply.github.com>
2023-05-27 13:37:55 +0200

    Update to match guide

--- scripts/pre/change_kernel.sh
+++ scripts/pre/change_kernel.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-rpm-ostree cliwrap install-to-root /
-wget -O /etc/yum.repos.d/linux-surface.repo https://pkg.surfacelinux.com/fedora/linux-surface.repo
+#rpm-ostree cliwrap install-to-root /
+sudo wget -O /etc/yum.repos.d/linux-surface.repo \
+          https://pkg.surfacelinux.com/fedora/linux-surface.repo
 cd /tmp
+mkdir surface-linux
+cd surface-linux
 wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm
-rpm-ostree override replace ./*.rpm
+rpm-ostree override replace ./*.rpm \
+           --remove kernel-core \
+           --remove kernel-modules \
+           --remove kernel-modules-extra \
+           --remove libwacom \
+           --remove libwacom-data \
+           --install kernel-surface \
+           --install iptsd \
+           --install libwacom-surface \
+           --install libwacom-surface-data
 rpm-ostree install surface-secureboot
