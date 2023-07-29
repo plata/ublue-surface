b371b1b6cc4b0890373c2654084327d2ac976a0f
plata <plata@users.noreply.github.com>
2023-07-29 10:51:40 +0200

    Do not download .rpm

--- scripts/pre/change_kernel.sh
+++ scripts/pre/change_kernel.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-#rpm-ostree cliwrap install-to-root /
+
 sudo wget -O /etc/yum.repos.d/linux-surface.repo \
           https://pkg.surfacelinux.com/fedora/linux-surface.repo
-cd /tmp
-mkdir surface-linux
-cd surface-linux
-wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm
-rpm-ostree override replace ./*.rpm \
-           --remove kernel-core \
-           --remove kernel-devel-matched \
-           --remove kernel-modules \
-           --remove kernel-modules-extra \
-           --remove libwacom \
-           --remove libwacom-data \
-           --install kernel-surface \
-           --install iptsd \
-           --install libwacom-surface \
-           --install libwacom-surface-data
+rpm-ostree cliwrap install-to-root / && \
+rpm-ostree override replace https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm
 rpm-ostree install surface-secureboot
