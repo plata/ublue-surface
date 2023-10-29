fe9f9a4743badceeb5617105fc5b90dbf00cb3c9
plata <plata@users.noreply.github.com>
2023-10-29 17:54:15 +0100

    Try without sudo

--- config/scripts/change_kernel.sh
+++ config/scripts/change_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo wget https://pkg.surfacelinux.com/fedora/linux-surface.repo -P /etc/yum.repos.d
-sudo wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm -O /tmp/surface-kernel.rpm
+wget https://pkg.surfacelinux.com/fedora/linux-surface.repo -P /etc/yum.repos.d
+wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm -O /tmp/surface-kernel.rpm
 rpm-ostree cliwrap install-to-root
 rpm-ostree override replace /tmp/surface-kernel.rpm \
            --remove kernel-core \
