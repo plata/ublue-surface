e9e546901a29ffcb66464e9caac6027c47753003
plata <plata@users.noreply.github.com>
2023-10-31 14:31:43 +0100

    Update configure_sddm.sh

--- config/scripts/configure_sddm.sh
+++ config/scripts/configure_sddm.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
 sed -i 's/layer-shell/layer-shell,QT_FONT_DPI=144/g' /usr/lib/sddm/sddm.conf.d/plasma-wayland.conf
