108ac8d8f906accc8eaa868da89bcda0467c1420
plata <plata@users.noreply.github.com>
2023-10-31 14:43:56 +0100

    Exit build with error if a script fails (#46)

--- config/scripts/change_kernel.sh
+++ config/scripts/change_kernel.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
 wget https://pkg.surfacelinux.com/fedora/linux-surface.repo -P /etc/yum.repos.d
 wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm -O /tmp/surface-kernel.rpm
 rpm-ostree cliwrap install-to-root
