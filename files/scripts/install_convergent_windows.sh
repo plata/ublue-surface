2886697312b6519e62edc537c884f278b6e64752
plata <plata@users.noreply.github.com>
2025-01-03 12:27:10 +0100

    Delete files/scripts/install_convergent_windows.sh

--- files/scripts/install_convergent_windows.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-cd /tmp
-curl -o convergentwindows-v1.0.tar.gz https://invent.kde.org/plata/convergentwindows/-/archive/v1.0/convergentwindows-v1.0.tar.gz
-tar -xf convergentwindows-v1.0.tar.gz
-mv convergentwindows-v1.0 /usr/share/kwin/scripts/convergentwindows
